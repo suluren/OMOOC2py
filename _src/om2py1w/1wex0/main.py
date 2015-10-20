25200768b2323996177d78847c37c90e00ebf3c9
suluren <suluren@gmail.com>
2015-10-20 13:30:46 +0800

    datebook 0.2

--- _src/om2py1w/1wex0/main.py
+++ _src/om2py1w/1wex0/main.py
@@ -8,28 +8,30 @@ def main():
         writing()    
     else:
         init_file = open('datebook.txt', 'w')
-        init_file.write("hello~")
+        init_file.write("hello~\n")
         init_file.close()
         
         writing()
 
 def writing():
     diary = []
-    localtime = time.strftime("%Y-%m-%d %X")
-
+    localtime = []
     line = "test"
+    
     while len(line) != 0:  
         line = raw_input("> ")  
-        diary.append(line + '\t' + localtime + '\n')
+        diary.append(line + '\n')
 
+    localtime.append(time.strftime("%Y-%m-%d %X")) 
     export = open('datebook.txt', 'a+')
-    export.writelines(diary)
+    export.writelines(diary + localtime)
     export.close()
     
 def reading():
     diary = []
     readbook = open('datebook.txt')
     print readbook.read()
+    readbook.close()
 
 if __name__=='__main__':
 	main()
